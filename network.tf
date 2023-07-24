e1ee3b91223c683da614c8a7ff60e73197b0e25a
JeffGiroux <thejeffgiroux@gmail.com>
2023-07-24 13:04:28 -0700

    mgmt nsg updates

--- network.tf
+++ network.tf
@@ -45,6 +45,45 @@ resource "azurerm_network_security_group" "mgmt" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
+  security_rule {
+    name                       = "allow_SSH"
+    description                = "Allow SSH access"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = var.adminSrcAddr
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow_HTTPS"
+    description                = "Allow HTTPS access"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = var.adminSrcAddr
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow_RDP"
+    description                = "Allow RDP access"
+    priority                   = 130
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = var.adminSrcAddr
+    destination_address_prefix = "*"
+  }
+
   tags = {
     owner = var.resourceOwner
   }
